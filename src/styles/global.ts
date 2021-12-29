5d2ff733a9aa8b03b96d5b69df95dfeaf8ba9d0a
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2021-12-29 12:59:30 -0300

    Add background color to body

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -10,6 +10,7 @@ export const GlobalStyles = createGlobalStyle`
 
   body{
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background: #fff;
   }
 
   html {
