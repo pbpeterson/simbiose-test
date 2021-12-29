055737afcd53234b214d5896a5264eb4699b48cc
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2021-12-29 13:21:03 -0300

    Add project mocks

---
+++ src/components/Project/mock.ts
@@ -0,0 +1,22 @@
+import { ProjectProps } from '.'
+
+export default [
+  {
+    title: 'Projeto Teste',
+    city: 'São Paulo',
+    uf: 'SP',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit provident amet quam facere debitis omnis tenetur ullam eos perspiciatis repellat.',
+    approved: 300,
+    captured: 500,
+  },
+  {
+    title: 'Projeto Teste 2',
+    city: 'Minas Gerais',
+    uf: 'MG',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit provident amet quam facere debitis omnis tenetur ullam eos perspiciatis repellat.',
+    approved: 100,
+    captured: 200,
+  },
+] as ProjectProps[]
