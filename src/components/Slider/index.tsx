b5f016c08928a7722a52248c4bb9c35ed116d70a
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2021-12-29 12:29:04 -0300

    Add Slider

---
+++ src/components/Slider/index.tsx
@@ -0,0 +1,25 @@
+import { forwardRef } from 'react'
+import SlickSlider, { Settings } from 'react-slick'
+
+import * as S from './styles'
+
+export type SliderSettings = Settings
+
+export type SliderProps = {
+  children: React.ReactNode
+  settings: SliderSettings
+}
+
+const Slider: React.ForwardRefRenderFunction<SlickSlider, SliderProps> = (
+  // eslint-disable-next-line react/prop-types
+  { children, settings },
+  ref
+) => (
+  <S.Wrapper>
+    <SlickSlider ref={ref} {...settings}>
+      {children}
+    </SlickSlider>
+  </S.Wrapper>
+)
+
+export default forwardRef(Slider)
